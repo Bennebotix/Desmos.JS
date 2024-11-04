3d85a9039c1e99c501db297644e1cbaeef59db02
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-11-04 15:38:39 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 class Calculator {
-  constructor(mode = 'e') {
+  constructor(eqs = [], mode = 'e') {
     const preview = {actions: !0, expressions: !1, settingsMenu: !1};
     const editor = {actions: !0};
 
@@ -7,4 +7,26 @@ class Calculator {
     var calculator = Desmos.GraphingCalculator(calcElmnt, mode == 'e' ? editor : preview);
     document.body.appendChild(calcElmnt);
   }
+}
+
+new PlainEquation {
+  construcctor(opts) {
+    var defaults = {};
+    opts = fillDefaults(opts, defaults);
+    this.type = "expression";
+    this.id = Math.random() * 1000000000;
+    this.color = opts.c;
+    this.latex = opts.l;
+    this.ls = opts.ls;
+    
+  }
+}
+
+
+function fillDefaults(a, b) {
+  var c = {};
+  for (let key in b) {
+      c[key] = (a.hasOwnProperty(key) ? a : b)[key];
+  }
+  return c;
 }
