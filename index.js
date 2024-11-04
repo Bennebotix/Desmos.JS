75912232c5f3b4b3443e956070139fbfb10423a3
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-11-04 15:21:40 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 class Calculator {
-  constructor() {
+  constructor(mode = 'e') {
     const preview = {actions: !0, expressions: !1, settingsMenu: !1};
     const editor = {actions: !0};
 
-    var calcElmnt = document.querySelector
-    var calculator = Desmos.GraphingCalculator(calcElmnt, editor);
+    var calcElmnt = document.createElement('div');
+    var calculator = Desmos.GraphingCalculator(calcElmnt, mode == 'e' ? editor : preview);
+    document.body.appendChild(caclElmnt);
   }
 }
